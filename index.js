e013a0e2dee95f1dcab8e54067c53d2ae5b2eed0
Joakimrocks <joakimfabio@gmail.com>
2019-06-11 15:35:23 +0300

    installed nodemon and hid credentials

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const pool = mysql.createPool({
 
-    host: "localhost",
-    user: "joakim",
-    password: "password",
-    database: "library"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 })
 
 // app.get("/", function(req, res) {
@@ -20,7 +21,7 @@ app.get("/api/students", (req, res) => {
     pool.query("SELECT address, firstname FROM students", (error, rows) => {
         if (error) {
             return res.status(500).json({ error });
-        
+
         }
 
         res.json(rows);
