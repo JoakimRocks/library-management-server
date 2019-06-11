0e640edb61e48e78406b3054a93f6a21ce63d946
Joakimrocks <joakimfabio@gmail.com>
2019-06-11 13:55:13 +0300

    server connection

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const pool = mysql.createPool({
 
-app.get("/", function(req, res) {
-    //logic
-
-    res.json({ message: "Hello Joakim" });
-
-
+    host: "localhost",
+    user: "joakim",
+    password: "password",
+    database: "library"
 })
 
+// app.get("/", function(req, res) {
+//     //logic
+
+//     res.json({ message: "Hello Joakim" });
+
+
+// })
+app.get("/api/students", (req, res) => {
+    pool.query("SELECT address, firstname FROM students", (error, rows) => {
+        if (error) {
+            return res.status(500).json({ error });
+        
+        }
+
+        res.json(rows);
+    });
+
+});
 app.listen(9000, function() {
 
     console.log("app listening to port 9000");
